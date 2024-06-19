5831781c5f5500a80afe876401d2764cbff76ace
Ausslanderr <paciencia.2001@gmail.com>
2024-06-18 21:49:04 -0300

    feat: melhorando os hiperparâmetros

--- treino_local/treinamento_local_sklearn_xgboost.py
+++ treino_local/treinamento_local_sklearn_xgboost.py
@@ -1,24 +1,28 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-import xgboost
-from xgboost import XGBClassifier  # Importando o XGBClassifier
+from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, accuracy_score
 
-# Carregar o CSV 
+# Carregar o CSV
 df = pd.read_csv('csv_config/modified_hotel_reservations.csv')
 
-#Testando sem as colunas id e status
+# Testando sem as colunas id e status
 df = df.drop(columns=['Booking_ID', 'booking_status'])
+
+# Adicionar a nova coluna
+df['no_total_people'] = df['no_of_adults'] + df['no_of_children']
+df['no_total_nights'] = df['no_of_weekend_nights'] + df['no_of_week_nights']
 
 # Separar as features e o label
 X = df.drop('label_avg_price_per_room', axis=1)
 y = df['label_avg_price_per_room']
 
 # Ajustando os rótulos da variável alvo para começarem de 0
-y = y-1
+y = y - 1
+
 # Identificar colunas categóricas e numéricas
 categorical_cols = X.select_dtypes(include=['object']).columns
 numeric_cols = X.select_dtypes(include=['number']).columns
@@ -37,16 +41,24 @@ preprocessor = ColumnTransformer(
 # Criar o pipeline incluindo o pré-processador e o modelo com XGBClassifier
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('classifier', XGBClassifier(n_estimators=1000, random_state=42))  # Usando XGBClassifier
-]) # Os estimadores são arvores de decisão
+    ('classifier', XGBClassifier(
+        n_estimators=300,
+        max_depth=9,
+        learning_rate=0.1,
+        subsample=0.8,
+        colsample_bytree=1.0,
+        random_state=42))
+])
+
 
 # Dividir os dados em conjuntos de treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-# Treinar o modelo com XGBoost
+# Treinando o modelo com GridSearchCV
 model.fit(X_train, y_train)
 
-# Fazer previsões e avaliar o modelo com XGBoost
+
+# Prever nos dados de teste e calcular a acurácia com o melhor modelo
 y_pred = model.predict(X_test)
 
 print("Accuracy:", accuracy_score(y_test, y_pred))
